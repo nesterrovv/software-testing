bf904b293275a77a01f1a3da37a0e2ba9f46d5a2
nesterrovv <nesterov.iv2018@gmail.com>
2023-02-20 13:40:32 +0300

    added 1st step of work demonstration to main method

--- lab1/src/main/java/com/nesterrovv/Main.java
+++ lab1/src/main/java/com/nesterrovv/Main.java
@@ -4,8 +4,25 @@ import com.nesterrovv.sinePowerSeries.SinePowerSeriesCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // todo (-inf; -36] V [36; +inf) not calculates
-        System.out.println("RES: " + SinePowerSeriesCalculator.calculateSinePowerSeries(10000, 100));
+        System.out.format("My calculation:                         sin(%d) = %f\n" +
+                        "Value calculated via built-in function: sin(%d) = %f\n\n",
+                42, SinePowerSeriesCalculator.calculateSinePowerSeries(42), 42,
+                Math.sin(Math.toRadians(42)));
+        System.out.format("My calculation:                         sin(%d) = %f\n" +
+                        "Value calculated via built-in function: sin(%d) = %f\n\n",
+                -42, SinePowerSeriesCalculator.calculateSinePowerSeries(-42), -42,
+                Math.sin(Math.toRadians(-42)));
+        System.out.format("My calculation:                         sin(%d) = %f\n" +
+                        "Value calculated via built-in function: sin(%d) = %f\n\n",
+                0, SinePowerSeriesCalculator.calculateSinePowerSeries(0), 0,
+                Math.sin(Math.toRadians(0)));
+        System.out.format("My calculation:                         sin(%d) = %f\n" +
+                        "Value calculated via built-in function: sin(%d) = %f\n\n",
+                1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(1234567), 1234567,
+                Math.sin(Math.toRadians(1234567)));
+        System.out.format("My calculation:                         sin(%d) = %f\n" +
+                        "Value calculated via built-in function: sin(%d) = %f\n\n",
+                1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(-1234567), -1234567,
+                Math.sin(Math.toRadians(-1234567)));
     }
 }
