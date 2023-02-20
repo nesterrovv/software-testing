dc1bca0d9ca7680515645da8f2678767d6516ff4
nesterrovv <nesterov.iv2018@gmail.com>
2023-02-20 14:04:27 +0300

    output formatting fixes

--- lab1/src/main/java/com/nesterrovv/Main.java
+++ lab1/src/main/java/com/nesterrovv/Main.java
@@ -7,25 +7,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.format("My calculation:                         sin(%d) = %f\n" +
+        System.out.printf("My calculation: %23s sin(%d) = %f\n" +
                         "Value calculated via built-in function: sin(%d) = %f\n\n",
-                42, SinePowerSeriesCalculator.calculateSinePowerSeries(42), 42,
+                "",42, SinePowerSeriesCalculator.calculateSinePowerSeries(42), 42,
                 Math.sin(Math.toRadians(42)));
-        System.out.format("My calculation:                         sin(%d) = %f\n" +
+        System.out.printf("My calculation: %23s sin(%d) = %f\n" +
                         "Value calculated via built-in function: sin(%d) = %f\n\n",
-                -42, SinePowerSeriesCalculator.calculateSinePowerSeries(-42), -42,
+                "", -42, SinePowerSeriesCalculator.calculateSinePowerSeries(-42), -42,
                 Math.sin(Math.toRadians(-42)));
-        System.out.format("My calculation:                         sin(%d) = %f\n" +
+        System.out.printf("My calculation: %23s sin(%d) = %f\n" +
                         "Value calculated via built-in function: sin(%d) = %f\n\n",
-                0, SinePowerSeriesCalculator.calculateSinePowerSeries(0), 0,
+                "", 0, SinePowerSeriesCalculator.calculateSinePowerSeries(0), 0,
                 Math.sin(Math.toRadians(0)));
-        System.out.format("My calculation:                         sin(%d) = %f\n" +
+        System.out.printf("My calculation: %23s sin(%d) = %f\n" +
                         "Value calculated via built-in function: sin(%d) = %f\n\n",
-                1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(1234567), 1234567,
+                "", 1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(1234567), 1234567,
                 Math.sin(Math.toRadians(1234567)));
-        System.out.format("My calculation:                         sin(%d) = %f\n" +
+        System.out.printf("My calculation: %23s sin(%d) = %f\n" +
                         "Value calculated via built-in function: sin(%d) = %f\n\n",
-                1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(-1234567), -1234567,
+                "", -1234567, SinePowerSeriesCalculator.calculateSinePowerSeries(-1234567), -1234567,
                 Math.sin(Math.toRadians(-1234567)));
 
         double[] array = {-1, -5, -7, -13, -234, 777, 0, 0};
