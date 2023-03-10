e26e4b69bb42543b0927ed206d831671727970e4
nesterrovv <nesterov.iv2018@gmail.com>
2023-03-10 09:25:55 +0300

    added test coverage for 3rd step

---
+++ lab1/src/test/java/com/nesterrovv/pojosmodel/PojosModelTest.java
@@ -0,0 +1,50 @@
+package com.nesterrovv.pojosModel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PojosModelTest {
+
+    @Test
+    public void checkAttack() {
+        Galaxy sample = new Galaxy();
+        sample.setLifeExpectancyFactor(200);
+        new Entity().attack(sample);
+        long expected = 100;
+        Assert.assertEquals(expected, sample.getLifeExpectancyFactor());
+    }
+
+    @Test
+    public void checkFirstCaseOfAnalyze() {
+        Galaxy first = new Galaxy();
+        first.setLifeExpectancyFactor(100);
+        Galaxy second = new Galaxy();
+        second.setLifeExpectancyFactor(200);
+        String test = new World().analyze();
+        String expected = "Retribution awaits the galaxy 2";
+        Assert.assertEquals(expected, test);
+    }
+
+    @Test
+    public void checkSecondCaseOfAnalyze() {
+        Galaxy first = new Galaxy();
+        first.setLifeExpectancyFactor(200);
+        Galaxy second = new Galaxy();
+        second.setLifeExpectancyFactor(100);
+        String test = new World().analyze();
+        String expected = "Retribution awaits the galaxy 1";
+        Assert.assertEquals(expected, test);
+    }
+
+    @Test
+    public void checkThirdCaseOfAnalyze() {
+        Galaxy first = new Galaxy();
+        first.setLifeExpectancyFactor(200);
+        Galaxy second = new Galaxy();
+        second.setLifeExpectancyFactor(200);
+        String test = new World().analyze();
+        String expected = "Retribution awaits the galaxy 2";
+        Assert.assertEquals(expected, test);
+    }
+
+}
