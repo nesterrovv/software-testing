953ddb8258da9b86debaf9d02d2bd5ec6c7e2df8
ZloyEgor <elopatov74@outlook.com>
2023-03-22 14:13:31 +0300

    Apply task for Vanya

--- lab2/src/test/java/TestFunction.java
+++ lab2/src/test/java/TestFunction.java
@@ -1,7 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.mockito.MockedStatic;
 import org.zloyegor.trigonometry.Sin;
 
 public class TestFunction {
@@ -10,8 +9,11 @@ public class TestFunction {
     public void mockSetup() {
         double accuracy = .1e-10;
 
+        // TODO: Надо сделать похожие заглушки для всех остальных тригонометрических функций и логарифмов в этом методе (пункт 4 в задании)
         Sin sinMock = Mockito.mock(Sin.class);
 
+        //Здесь по три точки в каждом классе эквивалентности (квадранты) + граничные значения + пара больших углов
+        //Стоит наверное по аналогии отрицательные углы добавить ещё
         Mockito.when(sinMock.calculate(0, accuracy)).thenReturn(0.);
         Mockito.when(sinMock.calculate(45, accuracy)).thenReturn(0.70710678118);
         Mockito.when(sinMock.calculate(60, accuracy)).thenReturn(0.86602540378);
@@ -26,12 +28,16 @@ public class TestFunction {
         Mockito.when(sinMock.calculate(405, accuracy)).thenReturn(0.70710678118);
         Mockito.when(sinMock.calculate(450, accuracy)).thenReturn(1d);
 
-        System.out.println(sinMock.calculate(135, accuracy));
-
     }
 
     @Test
-    public void test() {
-
+    public void testSin() {
+        /* TODO:
+            Как я понял, надо в каждом отдельном тесте провести интеграцию по одному модулю.
+            Берешь, описываешь фунцию полностью из заглушек, только синус здесь остается настоящий.
+            ассертишь в конце и усё.
+            По аналогии тестируешь по отдельности каждый из настоящих методов.
+            Можно еще бахнуть тест на все логарифмы и на все тригон. функции.
+         */
     }
 }
